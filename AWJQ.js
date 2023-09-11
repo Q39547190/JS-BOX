f21f35732e53726effcc8645d67f3de93a99d453
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-09-11 15:24:07 +0800

    更新 AWJQ.js

--- AWJQ.js
+++ AWJQ.js
@@ -426,8 +426,8 @@ async function shuaxin() {
       break;
     }
     await getdata();
-    var page = $cache.get("pg") + 1;
-    $cache.set("pg", page);
+    //var page = $cache.get("pg") + 1;
+    //$cache.set("pg", page);
   }
 }
 
@@ -436,7 +436,7 @@ async function shuaxin() {
 async function get_updata() {
     const resp = await $http.get($text.base64Decode("aHR0cHM6Ly9naHByb3h5LmNvbS9odHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vUTM5NTQ3MTkwL0pTLUJPWC9tYWluL0FXSlEtZ3guanNvbg=="));
     if(resp.response.statusCode === 200){
-        if (resp.data.version != "4.6") {
+        if (resp.data.version != "4.7") {
             $ui.alert({
                 title: "发现新版本 - " + resp.data.version,
                 message: resp.data.upexplain,
