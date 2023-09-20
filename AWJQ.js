5bdf22776daa3eb69e3f3f8056e14740c014363a
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-09-20 14:24:30 +0800

    更新 AWJQ.js

--- AWJQ.js
+++ AWJQ.js
@@ -436,7 +436,7 @@ async function shuaxin() {
 async function get_updata() {
     const resp = await $http.get($text.base64Decode("aHR0cHM6Ly9naHByb3h5LmNvbS9odHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vUTM5NTQ3MTkwL0pTLUJPWC9tYWluL0FXSlEtZ3guanNvbg=="));
     if(resp.response.statusCode === 200){
-        if (resp.data.version != "7.0") {
+        if (resp.data.version != "7.1") {
             $ui.alert({
                 title: "发现新版本 - " + resp.data.version,
                 message: resp.data.upexplain,
